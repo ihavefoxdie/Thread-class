07a71ff36ece651c099ae9f9a5cc42d4668a1970
purpeliz <vladislav.nekrasov123@gmail.com>
2022-12-15 18:56:44 +0300

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Testing
+{
+
+    public class ThreadExample
+    {
+        static Semaphore semaphoreObj = new(3, 3);
+
+        public static void Print()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.Name}: Text!");
+            }
+
+        }
+
+        public static void Loader()
+        {
+            while (true)
+            {
+                semaphoreObj.WaitOne();
+                //Console.WriteLine($"{Thread.CurrentThread.Name}");
+                //Thread.Sleep(1);
+                semaphoreObj.Release();
+
+            }
+        }
+
+        public static void Main()
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                Thread thread = new(Print)
+                {
+                    Name = "Thread " + i.ToString()
+                };
+                //thread.IsBackground= true;            //this line of code is most likely going to make it so that no thread ever manages to execute Print()
+                thread.Start();
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Thread hell = new(Loader)
+                {
+                    Name = "Thread " + i.ToString()
+                };
+                hell.Start();
+            }
+
+        }
+    }
+}
